e7cdeea982d9c32ac356edf3c281879f34434769
Ramlov <mathias.ramlov10@gmail.com>
2024-11-13 12:57:43 +0100

    Update app.py

--- sniffer/app.py
+++ sniffer/app.py
@@ -29,4 +29,4 @@ def stream():
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5556)
+    app.run(debug=True, host='0.0.0.0', port=5000)
