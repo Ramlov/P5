83bf8f4ede8e9f19695880f15a49acba7933492e
Ramlov <mathias.ramlov10@gmail.com>
2024-11-13 10:07:18 +0100

    Update sniff.py

--- sniff.py
+++ sniff.py
@@ -13,12 +13,6 @@ NETWORK_PROFILES = {
     "GOOD": {"min": 50, "max": 300}
 }
 
-PACKET_LOSS_SEQUENCES = {
-    0: {"index": -1, "sequence": [0, 0, 0, 0, 0, 1, 0, 1, 0]},
-    1: {"index": -1, "sequence": [0, 1, 0, 1, 0, 1, 0, 0, 0]},
-    2: {"index": -1, "sequence": [0, 1]}
-}
-
 PORT_RANGE = range(3000, 3029)
 
 def sniff_packets():
@@ -40,88 +34,36 @@ def print_port(pkt):
             if device_id < 0:
                 print("No Device ID Found")
                 return
-
-            fd = PACKET_LOSS_SEQUENCES[device_id]
-
-            index = fd["index"]
-            p = fd["sequence"]
-
-            # check if index is at the end
-            if index >= len(p): #reset counter
-                PACKET_LOSS_SEQUENCES[device_id]["index"] = 0
-                index = 0
-
-            # get the sequence p_i
-            p_i = p[index]
-
-            # increment index
-            PACKET_LOSS_SEQUENCES[device_id]["index"]+=1
-            print(PACKET_LOSS_SEQUENCES[device_id]["index"])
-            if p_i == 1: # drop package
-                print("Packet Loss!")
-                packet_callback(1, True, dst_ip, tcp_dport)
-                return
-
                 
             profile = fd_profiles[device_id]
             print(f"Network Profile for ID {device_id}: {profile}")
             profile_type = profile.get("profile")
             if profile_type in NETWORK_PROFILES:
+                packet_loss = {"GOOD": 2, "NORMAL": 5, "SLOW": 10}.get(profile_type, 0)
                 delay_range = NETWORK_PROFILES[profile_type]
                 delay = random.randint(delay_range["min"], delay_range["max"])
                 print(f"Chosen delay for profile {profile_type}: {delay} ms")
-                packet_callback(delay)
+                packet_callback(delay, packet_loss)
             else:
                 print(f"No network profile type found for {profile_type}")
 
-def packet_callback(delay, packet_loss=False, ip=None, port=None):
-    if packet_loss:
-        payload_loss = [
-            {
-                "ip": str(ip),
-                "port": port,
-                "destination_ip": "127.0.0.1",
-                "destination_port": 9,
-                "protocol": "tcp"
-            }
-        ]
-        response_loss = requests.post(
-            'http://192.168.1.8/api/ipredirect',
-            json=payload_loss
-        )
-        print(f"Response from redirect: {response_loss.text}")
-
-        # sleep(2)
-        # payload_remove_loss = [
-        #     {
-        #         "remove_ip_redirect": "1",
-        #         "ip": str(ip),
-        #         "port": port,
-        #         "destination_ip": "127.0.0.1",
-        #         "destination_port": 10,
-        #         "protocol": "tcp"
-        #     }
-        # ]
-        # response_clear = requests.post(
-        #     'http://192.168.1.8/api/ipredirect', json=payload_remove_loss
-        # )
-        # print(f"Response from clear: {response_clear.text}")
-        return
+def packet_callback(delay, packet_loss):
+    payload_loss = {
+                    "percent": packet_loss
+                    }
+    response_loss = requests.post(
+        'http://192.168.1.8/api/disciplines/packet_loss',
+        json=payload_loss
+    )
+    print(f"Response from redirect: {response_loss.text}")
     payload = {'milliseconds': delay}
     requests.post(
         'http://192.168.1.8/api/disciplines/packet_delay',
         json=payload
     )
 
-
-
 def get_id_from_port(port):
-    if port > 3009:
-        return port % 100
-    elif port >= 3000:
-        return port % 10
-    else:
-        return -1
+    return port - 3000
 
 if __name__ == "__main__":
     sniff_packets()
