eadcaa86ceae6d07691ca9bf8a09d478e5840615
Ramlov <mathias.ramlov10@gmail.com>
2024-11-19 09:29:38 +0100

    Delete data.py

--- relay-box/sniffer/data.py
+++
@@ -1,8 +0,0 @@
-# output_generator.py
-import time
-import random
-
-def generate_output():
-    while True:
-        yield f"Current value: {random.randint(1, 100)}\n"
-        time.sleep(1)  # Pause for a second
